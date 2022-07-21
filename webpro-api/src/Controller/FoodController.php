4654a0301eaa6bfdbb8a10111244fd1fd8561c64
dat118 <dat1182000@gmail.com>
2022-07-22 05:14:02 +0700

    add food controller

---
+++ webpro-api/src/Controller/FoodController.php
@@ -0,0 +1,69 @@
+<?php
+namespace Src\Controller;
+
+use Src\Model\Food;
+
+class FoodController {
+
+    private $db;
+    private $requestMethod;
+    private $userId;
+
+    private $personGateway;
+
+    public function __construct($db, $requestMethod, $userId)
+    {
+        $this->db = $db;
+        $this->requestMethod = $requestMethod;
+        $this->userId = $userId;
+
+        $this->food = new Food($db);
+    }
+
+    public function processRequest()
+    {
+        switch ($this->requestMethod) {
+            case 'GET':
+                if ($this->userId) {
+                    $response = $this->getFood($this->userId);
+                } else {
+                    $response = $this->getAllFoods();
+                };
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    private function getAllFoods()
+    {
+        $result = $this->food->findAll();
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function getFood($id)
+    {
+        $result = $this->food->find($id);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
+    }
+}
